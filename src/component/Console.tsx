f620b9362e9f90171c98daefd2991268baef9b7a
Ben Foxall <benfoxall@gmail.com>
2022-01-29 17:53:22 +0000

    Always snap to bottom

--- src/component/Console.tsx
+++ src/component/Console.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -19,8 +19,16 @@ interface ConsoleProps {
   history: string[];
 }
 
-export default function Console(props: ConsoleProps) {
+export default function Console({ history }: ConsoleProps) {
   const classes = useStyles();
+  const bottom = useRef<HTMLDivElement>(null);
 
-  return <Card className={classes.root}>{props.history.join("\n")}</Card>;
+  useEffect(() => bottom.current?.scrollIntoView(), [history]);
+
+  return (
+    <Card className={classes.root}>
+      {history.join("\n")}
+      <div ref={bottom} />
+    </Card>
+  );
 }
